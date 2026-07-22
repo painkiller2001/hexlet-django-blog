cff6a9921de5a07124df9aaf3d2f818fbc6cd54b
painkiller2001 <valeryfiadarenkou@EPBYMINW0510.localdomain>
2026-07-22 21:50:51 +0300

    Templates lesson

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,5 +3,10 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("article")
-
+    return render(
+            request,
+            "articles/index.html",
+            context={
+                "App_name": "article",
+                }
+            )
